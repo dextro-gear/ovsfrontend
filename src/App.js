32634e28dc5158b44c093bf3639da481d69ce6de
Abhishek Rajasundaram <abhishek.bhattar@outlook.com>
2021-04-19 15:43:49 +0530

    Import bootstrap.bundle.js in App.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 import {Route, BrowserRouter as Router, Link, Switch} from "react-router-dom";
 import LandingPage from "./components/pages/LandingPage";
 import "bootstrap/dist/css/bootstrap.min.css";
-import "bootstrap/dist/js/bootstrap.min.js";
+import "bootstrap/dist/js/bootstrap.bundle";
 
 function App() {
   return (
