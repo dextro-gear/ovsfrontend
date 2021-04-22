48894ab3bbad47deec1508016cd56adbbe3c0c1b
Rashi Gandhi <rashigandhi99@gmail.com>
2021-04-22 13:04:08 +0530

    Add cast vote page

--- src/App.js
+++ src/App.js
@@ -3,14 +3,18 @@ import {Route, BrowserRouter as Router, Link, Switch} from "react-router-dom";
 import LandingPage from "./components/pages/LandingPage";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle";
+import CastNewVote from './components/pages/votes/CastNewVote';
 
 function App() {
   return (
     <Router>
       <Switch>
         <Route exact path="/" component={LandingPage}/>
+        <Route exact path="/castVote" component={CastNewVote}/>
       </Switch>
+     
     </Router>
+    
   );
 }
 
--- src/components/pages/LandingPage.js
+++ src/components/pages/LandingPage.js
@@ -1,28 +1,29 @@
-import React, { Component } from 'react';
-import {Link} from "react-router-dom";
-
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import CastNewVote from "./votes/CastNewVote";
 
 class LandingPage extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
 
-    constructor(props) {
-        super(props);
-        this.state = {};
-    }
-
-    render() { 
-        return (
-            <div>
-                <nav className="navbar navbar-dark navbar-expand-lg bg-dark">
-                    <div className="container-fluid">
-                        <Link className="navbar-brand" href="/">Online Voting System</Link>
-                    </div>
-                </nav>
-                <div className="container-fluid text-center px-4 py-5">
-                        <h1 className="display-4">Welcome to the Online Voting System!</h1>
-                    </div>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div>
+        <nav className="navbar navbar-dark navbar-expand-lg bg-dark">
+          <div className="container-fluid">
+            <Link className="navbar-brand" href="/">
+              Online Voting System
+            </Link>
+          </div>
+        </nav>
+        <div className="container-fluid text-center px-4 py-5">
+          <h1 className="display-4">Welcome to the Online Voting System!</h1>
+        </div>
+      </div>
+    );
+  }
 }
- 
+
 export default LandingPage;
