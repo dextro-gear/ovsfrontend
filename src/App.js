b107938c8e807b8b7569066f27c4220467e54161
Abhishek Rajasundaram <abhishek.bhattar@outlook.com>
2021-04-22 13:26:03 +0530

    Add links to landing page

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-import './App.css';
 import {Route, BrowserRouter as Router, Link, Switch} from "react-router-dom";
 import LandingPage from "./components/pages/LandingPage";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle";
+import './App.css';
 
 function App() {
   return (
--- src/components/pages/LandingPage.js
+++ src/components/pages/LandingPage.js
@@ -10,16 +10,29 @@ class LandingPage extends Component {
     }
 
     render() { 
+        let arrayfun = (num1, num2) => num1.length + num2 ;
         return (
             <div>
-                <nav className="navbar navbar-dark navbar-expand-lg bg-dark">
+                <nav className="navbar navbar-dark navbar-expand-lg bg-dark py-3">
                     <div className="container-fluid">
                         <Link className="navbar-brand" href="/">Online Voting System</Link>
                     </div>
                 </nav>
                 <div className="container-fluid text-center px-4 py-5">
-                        <h1 className="display-4">Welcome to the Online Voting System!</h1>
+                    <h1 className="display-4">Welcome to the Online Voting System!</h1>
+                </div>
+                <div className="container text-center px-4 py-5">
+                    <div className="container">
+                    <Link className="btn btn-danger m-1 w-25 red-btn" id="one">
+                        <i class="fas fa-fist-raised"></i> Register as a voter
+                    </Link>
+                        <Link to="/castVote" className="btn btn-warning m-1 w-25 orange-btn">Cast your vote</Link>
                     </div>
+                    <div className="container d.flex justify-content-center">
+                        <Link className="btn btn-success m-1 w-25 green-btn">Register a Society</Link>
+                        <Link className="btn btn-dark m-1 w-25 blue-btn">View Election Results</Link>
+                    </div>
+                </div>
             </div>
         );
     }
