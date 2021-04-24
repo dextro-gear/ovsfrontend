aeac6162eb5883eb08203d0a37807120958df36a
Abhishek Rajasundaram <abhishek.bhattar@outlook.com>
2021-04-24 14:54:39 +0530

    Add ElectionResult page

--- src/App.js
+++ src/App.js
@@ -1,19 +1,20 @@
-import {Route, BrowserRouter as Router, Link, Switch} from "react-router-dom";
+import { Route, BrowserRouter as Router, Link, Switch } from "react-router-dom";
 import LandingPage from "./components/pages/LandingPage";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle";
-import CastNewVote from './components/pages/votes/CastNewVote';
-import './App.css';
+import CastNewVote from "./components/pages/votes/CastNewVote";
+import "./App.css";
+import ElectionResultPage from "./components/pages/ElectionResultPage";
 
 function App() {
   return (
     <Router>
       <Switch>
-        <Route exact path="/" component={LandingPage}/>
-        <Route exact path="/castVote" component={CastNewVote}/>
-      </Switch>   
+        <Route exact path="/" component={LandingPage} />
+        <Route exact path="/castVote" component={CastNewVote} />
+        <Route exact path="/result" component={ElectionResultPage} />
+      </Switch>
     </Router>
-    
   );
 }
 
---
+++ src/components/pages/electionresult/ResultCard.js
@@ -0,0 +1,42 @@
+import React, { Component } from "react";
+
+class ResultCard extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div className="col-md">
+        <div className="card">
+          <div
+            className={
+              this.props.candidate.result === "Elected"
+                ? "card-header text-white bg-success"
+                : this.props.candidate.result === "Unannounced"
+                ? "card-header text-black bg-warning"
+                : "card-header text-white bg-danger"
+            }
+          >
+            {this.props.candidate.result}
+          </div>
+          <div className="card-body">
+            <h5 className="card-title">{this.props.candidate.candidateName}</h5>
+            <p className="card-text">
+              Candidate ID: {this.props.candidate.candidateID}
+              <br />
+              Total Polled Votes: {this.props.candidate.totalPolledVotes}
+              <br />
+              Votes Obtained: {this.props.candidate.totalCandidateVotes}
+              <br />
+              Voting Percentage:{" "}
+              {this.props.candidate.candidatesVotesPercentage}
+            </p>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default ResultCard;
