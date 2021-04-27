54acfbe0e78fa7701b17cc1f202b2a3c763495e5
Abhishek Rajasundaram <abhishek.bhattar@outlook.com>
2021-04-27 12:55:39 +0530

    Remove stray code in App.js

--- src/App.js
+++ src/App.js
@@ -3,31 +3,13 @@ import LandingPage from "./components/pages/LandingPage";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle";
 import CastNewVote from "./components/pages/votes/CastNewVote";
-import ViewVoteDetailsByVoterId from "./components/pages/votes/ViewVoteDetailsByVoterId"
+import ViewVoteDetailsByVoterId from "./components/pages/votes/ViewVoteDetailsByVoterId";
 import "./App.css";
 import ElectionResultPage from "./components/pages/ElectionResultPage";
 import { addNewVote, searchByVoterId } from "./components/services/VoteService";
 
 function App() {
-
-
-/*  const data ={
-    voterID: 4,
-    candidateID: 6,
-    //candidateFirstName: "jhalak",
-    //candidateLastName: "gandhi",
-    societyID: 3
-  }
-*/
-/*  const  promise = addNewVote(data);
-  promise.then(response=>console.log("success",response.data))
-  .catch(error=>console.log("unsuccessful",error.message));
-  */
-const id=4;
-  const  promise = searchByVoterId(id);
-  promise.then(response=>console.log("success",response.data))
-  .catch(error=>console.log("unsuccessful",error.response.data));
- return (
+  return (
     <Router>
       <Switch>
         <Route exact path="/" component={LandingPage} />
