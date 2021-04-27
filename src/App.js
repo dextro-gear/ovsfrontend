6336cf539152c6d7bd08cea8488384f8308e7aed
rashigandhi99 <rashigandhi99@gmail.com>
2021-04-27 17:23:54 +0530

    service completed

--- src/App.js
+++ src/App.js
@@ -3,37 +3,26 @@ import LandingPage from "./components/pages/LandingPage";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle";
 import CastNewVote from "./components/pages/votes/CastNewVote";
-import ViewVoteDetailsByVoterCardId from "./components/pages/votes/ViewVoteDetailsByVoterCardId"
+import ViewVoteDetailsByVoterCardId from "./components/pages/votes/ViewVoteDetailsByVoterCardId";
 import "./App.css";
 import ElectionResultPage from "./components/pages/ElectionResultPage";
-import { addNewVote, searchByVoterCardId } from "./components/services/VoteService";
+import {
+  addNewVote,
+  searchByVoterCardId,
+} from "./components/services/VoteService";
 
 function App() {
-
-
-/*  const data ={
-    voterID: 4,
-    candidateID: 6,
-    //candidateFirstName: "jhalak",
-    //candidateLastName: "gandhi",
-    societyID: 3
-  }
-*/
-/*  const  promise = addNewVote(data);
-  promise.then(response=>console.log("success",response.data))
-  .catch(error=>console.log("unsuccessful",error.message));
-  */
-const id=4;
-  const  promise = searchByVoterCardId(id);
-  promise.then(response=>console.log("success",response.data))
-  .catch(error=>console.log("unsuccessful",error.response.data));
- return (
+  return (
     <Router>
       <Switch>
         <Route exact path="/" component={LandingPage} />
         <Route exact path="/castVote" component={CastNewVote} />
         <Route exact path="/result" component={ElectionResultPage} />
-        <Route exact path="/byVoterId" component={ViewVoteDetailsByVoterCardId} />
+        <Route
+          exact
+          path="/byVoterId"
+          component={ViewVoteDetailsByVoterCardId}
+        />
       </Switch>
     </Router>
   );
