50ff563294c1b06159bf00a48737b04a388e1c9a
s-nisha <nishi30599@gmail.com>
2021-04-29 11:31:08 +0530

    service added

--- src/components/pages/society/SocietyHome.js
+++ src/components/pages/society/SocietyHome.js
@@ -1,13 +1,42 @@
-import React from 'react'
-import AddSociety from './AddSociety'
-import DisplaySociety from './DisplaySociety'
-import GetSocietyById from './GetSocietyById'
+import { Link} from 'react-router-dom';
+import commonStyle from "./commonStyle.module.css";
 
 export default function SocietyHome() {
-    
+
     return (
-        <div>
-        <GetSocietyById/>
+            <div>
+            <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+                <Link to="/" className="navbar-brand" >
+                        Society Module
+                </Link>
+
+                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+
+                <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul className="navbar-nav mr-auto">
+                        
+                        <li className="nav-item">
+                            <Link to="/addSociety" className="nav-link" >
+                                <span>Add Society</span>
+                            </Link>
+                        </li>
+
+                        <li className="nav-item">
+                            <Link to="/societyById" className="nav-link" >
+                                <span>Get Society details by ID</span>
+                            </Link>
+                        </li>
+
+                    </ul>
+                </div>
+            </nav>
+            <div className={commonStyle.content}>
+            <div>
+                       <h3> Welcome to Society Module home.</h3>
+                    </div>
+                    </div>
         </div>
     )
 }
