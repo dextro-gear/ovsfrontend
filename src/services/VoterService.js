c6cce7259c9073144dff44b0396403a00e85189c
Ritik Raj Singh <ritikrajsingh7@gmail.com>
2021-04-30 11:20:57 +0530

    comments added

--- src/services/VoterService.js
+++ src/services/VoterService.js
@@ -2,13 +2,22 @@ import axios from 'axios';
 
 const baseUrl="http://localhost:8585";
     
-
+/**
+ * This function fetches the voter details by voterId card number
+ * @param {int} id -The voter cardId for whom the details are to be fetched.
+ * @returns all the voter details are fetched.
+ */
     function fetchVoter(id){
         const url=baseUrl+"/voters/"+id ; 
         const promise=axios.get(url);
         return promise;
       }
 
+/**
+ * This function adds voter to the database.
+ * @param {data} data is all the information of the voter.
+ * @returns {Promise} A promise of the response from the back-end.
+ */
     function addVoter(data) {
       const url = baseUrl + "/voters/add";
       
@@ -17,16 +26,14 @@ const baseUrl="http://localhost:8585";
       return promise;
     }
 
+/**
+ * This function is to display the voter data
+ * @returns {Promise} A promise of the response from the back-end.
+ */
       function displayVoter() {
       const url = baseUrl + "/voters/display";
       const promise =axios.get(url);
       return promise;
     }
 
-    function fetchAll(){
-      const url = baseUrl + "/voters";
-      const promise = axios.get(url);
-      return promise;
-  }
-
-export {fetchVoter, addVoter, displayVoter, fetchAll};
+export {fetchVoter, addVoter, displayVoter};
